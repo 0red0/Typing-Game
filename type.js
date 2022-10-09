cb965eea1c84768ebbabba4c8b7a1e5f53d7faa3
yhyawy <yhyawy1@gmail.com>
2022-10-09 18:27:16 +0200

    menus control from Escape

--- type.js
+++ type.js
@@ -33,21 +33,21 @@
 
 // Array Of Words
 const words = [
-   "Hello",
-   "Programming",
-   // "Code",
-   // "Javascript",
-   // "Town",
-   // "Country",
-   // "Testing",
-   // "Youtube",
-   // "Linkedin",
-   // "Twitter",
+  "Hello",
+  "Programming",
+  // "Code",
+  // "Javascript",
+  // "Town",
+  // "Country",
+  // "Testing",
+  // "Youtube",
+  // "Linkedin",
+  // "Twitter",
 ];
 const lvls = {
-   Easy: 5,
-   Normal: 3,
-   Hard: 2,
+  Easy: 5,
+  Normal: 3,
+  Hard: 2,
 };
 
 // Document Elements
@@ -75,48 +75,48 @@ let levelInfo;
 
 // Check localStorage for old score
 if (window.localStorage.getItem("scores")) {
-   arrScore = JSON.parse(localStorage.getItem("scores"));
-   arrScore.forEach((o) => {
-      appendScoreEle(o);
-   });
+  arrScore = JSON.parse(localStorage.getItem("scores"));
+  arrScore.forEach((o) => {
+    appendScoreEle(o);
+  });
 }
 // Check localStorage for Level and TimeLeft
 if (window.localStorage.getItem("level")) {
-   levelInfo = JSON.parse(localStorage.getItem("level"));
-   console.log(levelInfo.timing);
-   updateLvlTime(levelInfo);
+  levelInfo = JSON.parse(localStorage.getItem("level"));
+  console.log(levelInfo.timing);
+  updateLvlTime(levelInfo);
 } else {
-   // Default lvl and timeLeft values
-   let defaultLvl = "Hard";
-   selectBox.value = defaultLvl;
-   lvl.innerText = defaultLvl;
-   timeLeft.forEach((t) => {
-      t.innerText = lvls[defaultLvl];
-   });
+  // Default lvl and timeLeft values
+  let defaultLvl = "Hard";
+  selectBox.value = defaultLvl;
+  lvl.innerText = defaultLvl;
+  timeLeft.forEach((t) => {
+    t.innerText = lvls[defaultLvl];
+  });
 }
 function updateLvlTime(l) {
-   timeLeft.forEach((w) => {
-      w.innerText = l.timing;
-   });
-   lvl.innerText = l.level;
-   selectBox.value = l.level;
+  timeLeft.forEach((w) => {
+    w.innerText = l.timing;
+  });
+  lvl.innerText = l.level;
+  selectBox.value = l.level;
 }
 
 input.onpaste = function () {
-   return false;
+  return false;
 };
 
 // Update Level and time
 selectBox.onchange = function () {
-   timeLeft.forEach((w) => {
-      w.innerText = lvls[selectBox.value];
-   });
-   lvl.innerText = selectBox.value;
-   let lvlObj = {
-      level: selectBox.value,
-      timing: lvls[selectBox.value],
-   };
-   localStorage.setItem("level", JSON.stringify(lvlObj));
+  timeLeft.forEach((w) => {
+    w.innerText = lvls[selectBox.value];
+  });
+  lvl.innerText = selectBox.value;
+  let lvlObj = {
+    level: selectBox.value,
+    timing: lvls[selectBox.value],
+  };
+  localStorage.setItem("level", JSON.stringify(lvlObj));
 };
 
 // score count From
@@ -127,136 +127,139 @@ startBtn.addEventListener("click", everyTurn);
 startBtn.addEventListener("click", addInitialTime);
 // Press Enter to start
 window.addEventListener("keypress", (e) => {
-   if (e.key === "Enter") {
-      everyTurn();
-      addInitialTime();
-   }
+  if (e.key === "Enter") {
+    everyTurn();
+    addInitialTime();
+  }
 });
 
 function addInitialTime() {
-   if (!doorWedge) {
-      footerTime.innerText = lvls[selectBox.value] + 0;
-   }
-   doorWedge = true;
+  footerTime.innerText = lvls[selectBox.value] + 2;
 }
 
 //=====( FUNCTIONS SECTION )====\\
 
 function everyTurn() {
-   // Random word & Append it & Remove it from array
-   randoWord = words[Math.floor(Math.random() * words.length)];
-   theWord.innerText = randoWord;
-   console.log(randoWord);
-   words.splice(words.indexOf(randoWord), 1);
+  // Random word & Append it & Remove it from array
+  randoWord = words[Math.floor(Math.random() * words.length)];
+  theWord.innerText = randoWord;
+  console.log(randoWord);
+  words.splice(words.indexOf(randoWord), 1);
 
-   // Delete btn & Display the word
-   startBtn.style.display = "none";
-   theWord.style.display = "block";
-   input.focus();
+  // Delete btn & Display the word
+  startBtn.style.display = "none";
+  theWord.style.display = "block";
+  input.focus();
 
-   // Append Array of words to document
-   upcomingWords.innerHTML = "";
-   words.forEach((w) => {
-      let newWord = document.createElement("span");
-      newWord.innerText = w;
-      upcomingWords.append(newWord);
-   });
-   wordsLeft.innerText = words.length;
-   //
-   play();
+  // Append Array of words to document
+  upcomingWords.innerHTML = "";
+  words.forEach((w) => {
+    let newWord = document.createElement("span");
+    newWord.innerText = w;
+    upcomingWords.append(newWord);
+  });
+  wordsLeft.innerText = words.length;
+  //
+  play();
 }
 
 // Play the turn
 function play() {
-   footerTime.innerText = lvls[selectBox.value];
-   let fTime = setInterval(() => {
-      footerTime.innerText--;
-      if (footerTime.innerText == "0") {
-         clearInterval(fTime);
-         if (input.value.toLowerCase() == randoWord.toLowerCase()) {
-            input.value = "";
-            correctType.innerText++;
-            wordsLeft.innerText--;
-            // Check if Array empty
-            if (words.length == 0) {
-               result.style.display = "block";
-               result.innerText = "Well Done!";
-               wordsLeft.innerText = "0";
-               scoring();
-            } else {
-               input.value = "";
-               everyTurn();
-            }
-         } else {
-            result.style.display = "block";
-            result.classList.add("loss");
-            result.innerText = "Game Over!";
-            scoring();
-         }
+  footerTime.innerText = lvls[selectBox.value];
+  let fTime = setInterval(() => {
+    footerTime.innerText--;
+    if (footerTime.innerText == "0") {
+      clearInterval(fTime);
+      if (input.value.toLowerCase() == randoWord.toLowerCase()) {
+        input.value = "";
+        correctType.innerText++;
+        wordsLeft.innerText--;
+        // Check if Array empty
+        if (words.length == 0) {
+          result.style.display = "block";
+          result.innerText = "Well Done!";
+          wordsLeft.innerText = "0";
+          scoring();
+        } else {
+          input.value = "";
+          everyTurn();
+        }
+      } else {
+        result.style.display = "block";
+        result.classList.add("loss");
+        result.innerText = "Game Over!";
+        scoring();
       }
-   }, 1000);
+    }
+  }, 1000);
 }
 // Manage The Score
 function scoring() {
-   const scoreText = document.querySelector(
-      "footer>p:last-of-type"
-   ).textContent;
-   // console.log(scoreText);
-   addScoreToArr(scoreText);
+  const scoreText = document.querySelector("footer>p:last-of-type").textContent;
+  // console.log(scoreText);
+  addScoreToArr(scoreText);
 }
 // Manage LocalStorage array
 function addScoreToArr(name) {
-   const objScore = {
-      id: Date.now(),
-      theScore: name,
-   };
-   arrScore.push(objScore);
-   window.localStorage.setItem("scores", JSON.stringify(arrScore));
+  const objScore = {
+    id: Date.now(),
+    theScore: name,
+  };
+  arrScore.push(objScore);
+  window.localStorage.setItem("scores", JSON.stringify(arrScore));
 }
 // Prep the score time Stamp
 function getStamp(dd) {
-   let timePoint = new Date(dd);
-   let day = timePoint.getDate();
-   let month = timePoint.getMonth();
-   let year = timePoint.getFullYear();
-   let hour = timePoint.getHours();
-   hour = hour < 10 ? `0${hour}` : hour;
-   let minute = timePoint.getMinutes();
-   minute = minute < 10 ? `0${minute}` : minute;
-   let second = timePoint.getSeconds();
-   second = second < 10 ? `0${second}` : second;
-   return `${day}_${month}_${year} - ${hour}:${minute}:${second}`;
+  let timePoint = new Date(dd);
+  let day = timePoint.getDate();
+  let month = timePoint.getMonth();
+  let year = timePoint.getFullYear();
+  let hour = timePoint.getHours();
+  hour = hour < 10 ? `0${hour}` : hour;
+  let minute = timePoint.getMinutes();
+  minute = minute < 10 ? `0${minute}` : minute;
+  let second = timePoint.getSeconds();
+  second = second < 10 ? `0${second}` : second;
+  return `${day}_${month}_${year} - ${hour}:${minute}:${second}`;
 }
 // Append score elements
 function appendScoreEle(x) {
-   let p = document.createElement("p");
-   p.innerText = `${getStamp(x.id)} - ${x.theScore}`;
-   p.classList.add("hide");
-   scoreDiv.append(p);
+  let p = document.createElement("p");
+  p.innerText = `${getStamp(x.id)} - ${x.theScore}`;
+  p.classList.add("hide");
+  scoreDiv.append(p);
 }
 // Score Button
 scoreBtn.addEventListener("click", openScore);
 scoreBtn.addEventListener("click", hideClr);
 function openScore() {
-   for (const child of scoreDiv.children) {
-      child.classList.toggle("hide");
-   }
+  for (const child of scoreDiv.children) {
+    child.classList.toggle("hide");
+  }
 }
 // Hide score clear button
 function hideClr() {
-   clr.classList.toggle("hide");
+  clr.classList.toggle("hide");
 }
 // Clear Score Button
 clr.addEventListener("click", clearScore);
 function clearScore() {
-   localStorage.removeItem("scores");
-   scoreDiv.innerText = "";
-   clr.classList.toggle("hide");
+  localStorage.removeItem("scores");
+  scoreDiv.innerText = "";
+  clr.classList.toggle("hide");
 }
 // Info button
 infoBtn.addEventListener("click", openInfo);
 function openInfo() {
-   pInfo.forEach((p) => {
-      p.classList.toggle("hide");
-   });
+  pInfo.forEach((p) => {
+    p.classList.toggle("hide");
+  });
 }
+// Hide both menus with Escape
+window.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    hideClr();
+    openScore();
+    openInfo();
+  }
+});
